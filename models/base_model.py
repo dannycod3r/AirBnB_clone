6f3b9c78b87dc71d3964970b903bd9fdc3a46535
EmmanuelAdu <christorenzo20@gmail.com>
2023-10-10 08:18:47 +0100

    Update to BaseModel with kwargs instantiation

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,27 @@ from datetime import datetime
 class BaseModel:
     """Defines a parent class BaseModel"""
 
-    def __init__(self):
-        """Initializes Public instance Attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initializes Public instance Attributes
+
+        Args:
+             *args - list of arguments
+             **kwargs - dictionary representation of arguments
+        """
+        if kwargs is not None and kwargs != {}:
+            for k in kwargs:
+                if k == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[k] = kwargs[k]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the official string representation of object"""
