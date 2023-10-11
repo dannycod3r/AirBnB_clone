b14e2e73e2eab07d7fb46b10808da57cc384c308
EmmanuelAdu <christorenzo20@gmail.com>
2023-10-11 18:04:31 +0100

    Creating a FILESTORAGE SYSTEM

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -29,6 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns the official string representation of object"""
@@ -37,6 +39,7 @@ class BaseModel:
     def save(self):
         """Saves and update the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary of all keys and values of __dict__ instance"""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines new class FileStorage"""
+import json
+import datetime
+import os
+
+
+class FileStorage:
+    """Serializes instances to JSON file and deserializes JSON to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary object"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Stores a new object in the __objects dictionary using a key"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to JSON FILE to __file_path(file.json)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as op_file:
+            Ndict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(Ndict, op_file)
+
+    def reload(self):
+        """Deserializes JSON FILE to __objects(dict) if path exists"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(File.__file_path, "r", encoding="utf-8") as op_file:
+            convt_dict = json.load(op_file)
+        FileStorage.__objects = convt_dict
