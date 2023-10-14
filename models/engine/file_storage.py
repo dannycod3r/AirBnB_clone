8d08fc5ac7411aeac1293fd2e866b3c7f74312ec
dannycod3r <oseid7807@gmail.com>
2023-10-14 23:48:50 +0000

    Update class list to handle all other classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@ import json
 import datetime
 import os
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class FileStorage:
@@ -13,34 +19,39 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary object"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Stores a new object in the __objects dictionary using a key"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to JSON FILE to __file_path(file.json)"""
-        Ndict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as op_file:
+        Ndict = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(self.__file_path, "w", encoding="utf-8") as op_file:
             json.dump(Ndict, op_file)
 
     def reload(self):
         """Deserializes JSON FILE to __objects(dict) if path exists"""
-        if not os.path.isfile(FileStorage.__file_path):
+        if not os.path.isfile(self.__file_path):
             return
 
-        with open(FileStorage.__file_path, 'r') as file:
+        with open(self.__file_path, 'r') as file:
             data = json.load(file)
             for key, value in data.items():
                 class_name = key.split('.')[0]
                 cls = eval(class_name)
-                FileStorage.__objects[key] = cls(**value)
+                self.__objects[key] = cls(**value)
 
-    # def classes(self):
-    #     """Returns all Implemented classes"""
-    #     from models.base_model import BaseModel
-
-    #     classes = {"BaseModel": BaseModel}
-    #     return classes
+    def classes(self):
+        """return a list of class names"""
+        return {
+            'BaseModel': BaseModel, 
+            'User': User,
+            'Amenity': Amenity,
+            'City': City,
+            'State': State,
+            'Place': Place,
+            'Review': Review
+            }
